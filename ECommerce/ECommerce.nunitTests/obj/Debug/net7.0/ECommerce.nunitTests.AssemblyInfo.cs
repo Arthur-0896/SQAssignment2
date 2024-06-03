99c18188eadf465c2183077858149b5011dc9f0c
Arthur Tristram <arthur.tristram1@gmail.com>
2024-06-03 13:38:14 -0400

    Product class completed

--- ECommerce/ECommerce.nunitTests/obj/Debug/net7.0/ECommerce.nunitTests.AssemblyInfo.cs
+++ ECommerce/ECommerce.nunitTests/obj/Debug/net7.0/ECommerce.nunitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.nunitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed1f707633f52af3b990ce4292a60389d2977564")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.nunitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.nunitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ECommerce/ECommerce/Product.cs
+++ ECommerce/ECommerce/Product.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ECommerce
 {
-    internal class Product
+    public class Product
     {
+        private int ProductID { get; set; }
+        private string ProductName { get; set; }
+        private double Price { get; set; }
+        private int Stock { get; set; }
+
+        public void increaseStock(int count)
+        {
+            Stock += count;
+        }
+        public void decreaseStock(int count)
+        {
+            increaseStock(-count);
+        }
     }
 }
